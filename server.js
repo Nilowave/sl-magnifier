638e4f3594bb2114ec3c8d16b3ed4b47bd8b3b1b
Danilo Meulens <danilo@mediamonks.com>
2020-04-11 10:12:07 -0700

    index src removed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/src/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 var clicks = 0;
