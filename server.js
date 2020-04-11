82bbb548c728c0b9709a2b93e9ffcb602b1b2471
Danilo Meulens <danilo@mediamonks.com>
2020-04-11 10:06:51 -0700

    the beginning

---
+++ server.js
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/src/index.html');
+});
+
+var clicks = 0;
+let max = 100;
+
+let colors = ['red', 'blue', 'green'];
+let colorCount = 0;
+let color = colors[colorCount];
+
+function setAmount() {
+  let p = clicks <= max ? clicks/max : max;
+  if (clicks >= max) {
+    clicks = 0;
+    colorCount = colorCount >= 2 ? 2 : colorCount + 1;
+    color = colors[colorCount];
+  }
+  console.log(p)
+  return p
+}
+
+io.on('connection', function(socket) {
+  console.log("connect")
+  io.emit('show it', {color:color,value:setAmount()});
+  socket.on('click it', function(msg) {
+    clicks++;
+    // console.log(clicks)
+    io.emit('show it', {color:color,value:setAmount()});
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
